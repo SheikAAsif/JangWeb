c675280037654ac3439459590866f8b421381536
SheikAAsif <sheikh.azd@gmail.com>
2023-05-21 15:48:48 +0500

    vercel deployment file

--- JangWeb/settings.py
+++ JangWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xwqx40!kj_e)^z-vbbh0y%@&l-r2^9e1y$vxbz9q3*o4m_7roa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -134,3 +134,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR /"media"
 MEDIA_URL = "/media/"
+
+# for vercel deployment
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
